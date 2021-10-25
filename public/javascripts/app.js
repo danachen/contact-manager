2c3e71739cef751a6f15fb1d5d9ef83d78b4e260
Dana Chen <danaqchen@gmail.com>
2021-10-25 10:45:01 +0200

    crud functionalities

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -36,6 +36,10 @@ App = {
     this.template = this.render();
     document.querySelector('.contact-list-container').innerHTML = this.template(this.contactList);
   },
+
+  resetForm() {
+    // TODO: reset form
+  },
   
   // Main methods
   getAllContacts() {
@@ -54,18 +58,22 @@ App = {
 
   formValidation() {
     const contactElements = document.querySelectorAll('.contact');
-
+    let passed = true;
     for (const el of contactElements) {
+      console.log(el, el.checkValidity());
       if (!el.checkValidity()) {
         el.parentElement.lastElementChild.innerHTML = el.validationMessage;
+        passed = false;
       }
     }
+    return passed;
   },
 
   addContact() {
     const data = new FormData(this.form);
     let object = {};
     data.forEach((value, key) => object[key]=value);
+    data['id'] = this.currentContactId;
     const json = JSON.stringify(object);
 
     const request = new XMLHttpRequest();
@@ -73,7 +81,7 @@ App = {
     request.setRequestHeader('Content-Type', 'application/json');
     request.addEventListener('load', () => {
       if (request.status === 201) {
-        console.log('add successful');
+        this.resetForm();
       }
     })
     request.send(json);
@@ -82,70 +90,83 @@ App = {
   editContact() {
     const data = new FormData(this.form);
     let object = {};
+    const currentContactId = Number(this.currentContact['id']);
+    data.set('id', currentContactId);
     data.forEach((value, key) => object[key]=value);
     const json = JSON.stringify(object);
-    const currentContactId = Number(this.currentContact['id']);
 
+    console.log(json);
     const request = new XMLHttpRequest();
     request.open('PUT', this.url + `/api/contacts/${currentContactId}`);
-    request.setRequestHeader('Content-Type', 'application/json/');
+    request.setRequestHeader('Content-Type', 'application/json');
     request.addEventListener('load', () => {
       console.log(request.status);
-      if (request.status === 201  ) {
+      if (request.status === 201    ) {
         console.log('edit successful');
       }
     })
     request.send(json);
   },
 
-  loadCurrentContactInfo() {
-    this.showFormTemplate();
+  loadCurrentContactInfo(contact) {
     this.addContactBoolean = false;
     let form = document.getElementById('add');
-    [fullName, email, phone, tags] = [this.currentContact['full_name'], this.currentContact['email'], this.currentContact['phone_number'], this.currentContact['tags']];
+    form.hidden = false;
+    [fullName, email, phone, tags] = [contact['full_name'], contact['email'], contact['phone_number'], contact['tags']];
     form.querySelector('.contact-name').value = fullName;
     form.querySelector('.contact-email').value = email;
     form.querySelector('.contact-phone').value = phone;
     form.querySelector('.contact-tags').value = tags;
   },
 
-  getExistingContact(e) {
+  getExistingContact(e, callback) {
     const currentContactId = Number(e.target.parentElement.parentElement.parentElement.getAttribute('value'));
 
     const request = new XMLHttpRequest();
-    request.open('GET', this.url + `/api/contacts/${currentContactId}`);
+    request.open('GET', `/api/contacts/${currentContactId}`);
     request.setRequestHeader('Content-Type', 'application/json');
     request.addEventListener('load', () => {
       this.currentContact = JSON.parse(request.response);
-      this.loadCurrentContactInfo();
+      this.currentContactId = this.currentContact['id'];
+      callback(this.currentContact);
     })
+    request.send();
+  },
+
+  deleteContact(contact) {
+    const id = contact['id'];
+    const request = new XMLHttpRequest();
+    request.open('DELETE', `/api/contacts/${id}`);
+    request.addEventListener('load', () => {
+      console.log(this.url);
+      if (request.status === 204) {
+        console.log('delete successful');
+      }
+    });
     request.send();
   },
 
   handleClick(e) {
     e.preventDefault();
-    // TODO: check why handleClick is no longer working with submission or resubmission(edit)
+    console.log(e.target.id);
     if (e.target.id === "add-contact") {
-      // if (this.addContactBoolean) {
-        if (Array.from(this.addFormElements).indexOf(e.target) > -1) {
-          this.showFormTemplate();
-        } else if (Array.from(this.cancelFormElement).indexOf(e.target) > -1) {
-          this.hideFormTemplate();
-        } else if (this.submitForm === e.target) {
-          this.formValidation();
+      this.showFormTemplate();
+    } else if (e.target.id === "cancel") {
+      this.hideFormTemplate();
+    } else if (e.target === this.submitForm) {
+      if (this.formValidation()) {
+        if (this.addContactBoolean) {
           this.addContact();
+        } else {
+          this.editContact();
+          this.addContactBoolean = true;
         }
-      } 
-      // else {
-      //   console.log('edit button clicked');
-      //   this.editContact();
-      // }
-    // } 
-    else if (e.target.id === 'update-contact') {
-      console.log('handleClick(e) works')
-      this.getExistingContact(e);
+      };
+    } else if (e.target.id === 'update-contact') {
+      console.log('update handle works');
+      this.getExistingContact(e, this.loadCurrentContactInfo);
     } else if (e.target.id === 'delete-contact') {
-      console.log('delete contact');
+      this.getExistingContact(e, this.deleteContact);
     }
   },
 
@@ -160,7 +181,7 @@ App = {
     document.addEventListener('keypress', this.handleEnter.bind(this));
   },
 
-  init() {
+  async init() {
     this.addFormElements = document.querySelectorAll('.add-contact');
     this.submitForm = document.getElementById('submit');
     this.cancelFormElement = document.querySelectorAll('.cancel');
@@ -174,6 +195,7 @@ App = {
     this.addContactBoolean = true;
     this.bindEvents();
     this.getAllContacts();
+    // this.getForm = await this.showFormTemplate();
   },
 }
 
