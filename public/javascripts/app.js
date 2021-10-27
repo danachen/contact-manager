258f3197a99d12adb536d8ece383c2c2fb9e7a71
Dana Chen <danaqchen@gmail.com>
2021-10-27 22:32:45 +0200

    search and tag features

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,20 +1,42 @@
 App = {
   // Helper methods
+  debounce(func, delay) {
+    let timeout;
+    return (...args) => {
+      if (timeout) { clearTimeout(timeout) }
+        timeout = setTimeout(() => func.apply(null, args), delay);
+    };
+  },
+
+  renderTags() {
+    for (let tag of Object.keys(this.tagList)) {
+      const li = document.createElement('li');
+      li.classList.add('list-of-tags');
+      li.style.backgroundColor='white';
+      li.innerText = tag;
+      this.tagCloudSearch.append(li);
+    }
+  },
+
   getTags() {
-    for (const contact of this.contactList) {
-      if (!contact['tags']) {
-        continue;
-      } else {
-        let tags = contact['tags'].split(', ');
-        for (let tag of tags) {
-          // TODO: need to delete the entries with tags that are ', ' separated, then it should work
-          if (this.tagList.indexOf(tag) < 0) {
-            this.tagList.push(tag);
+    const request = new XMLHttpRequest();
+    request.open('GET', this.url + "/api/contacts");
+    request.responseType = 'json';
+
+    request.addEventListener('load', () => {
+      let listOfTags = Array.from(request.response).map(item => item.tags);
+      for (let tagStr of listOfTags) {
+        let tagArr = tagStr.split(',');
+        for (let tag of tagArr) {
+          if (Object.keys(this.tagList).indexOf(tag) < 0) {
+            this.tagList[tag] = false;
           }
         }
       }
-     
-    }
+      this.renderTags();
+    })
+
+    request.send();
   },
 
   showFormTemplate() {
@@ -27,20 +49,15 @@ App = {
   },
   
   render() {
-    let contactTemplate = document.getElementById('contact-template').innerHTML;
-    let compiledContactTemplate = Handlebars.compile(contactTemplate);
+    let compiledContactTemplate = Handlebars.compile(this.contactTemplate);
     return compiledContactTemplate;
   },
 
-  renderContacts() {
+  renderContacts(contacts) {
     this.template = this.render();
-    document.querySelector('.contact-list-container').innerHTML = this.template(this.contactList);
+    document.querySelector('.contact-list-container').innerHTML = this.template(contacts);
   },
 
-  resetForm() {
-    // TODO: reset form
-  },
-  
   // Main methods
   getAllContacts() {
     const request = new XMLHttpRequest();
@@ -49,9 +66,8 @@ App = {
 
     request.addEventListener('load', () => {
       this.contactList = request.response;
-      this.renderContacts();
+      this.renderContacts(this.contactList);
       this.getTags();
-      document.querySelector('.no-contact-message').style.visibility="none";
     })
     request.send();
   },
@@ -60,7 +76,6 @@ App = {
     const contactElements = document.querySelectorAll('.contact');
     let passed = true;
     for (const el of contactElements) {
-      console.log(el, el.checkValidity());
       if (!el.checkValidity()) {
         el.parentElement.lastElementChild.innerHTML = el.validationMessage;
         passed = false;
@@ -81,7 +96,7 @@ App = {
     request.setRequestHeader('Content-Type', 'application/json');
     request.addEventListener('load', () => {
       if (request.status === 201) {
-        this.resetForm();
+        window.location.href = "/"
       }
     })
     request.send(json);
@@ -95,14 +110,13 @@ App = {
     data.forEach((value, key) => object[key]=value);
     const json = JSON.stringify(object);
 
-    console.log(json);
     const request = new XMLHttpRequest();
     request.open('PUT', this.url + `/api/contacts/${currentContactId}`);
     request.setRequestHeader('Content-Type', 'application/json');
     request.addEventListener('load', () => {
-      console.log(request.status);
-      if (request.status === 201    ) {
+      if (request.status === 201) {
         console.log('edit successful');
+        window.location.href = "/"
       }
     })
     request.send(json);
@@ -138,7 +152,6 @@ App = {
     const request = new XMLHttpRequest();
     request.open('DELETE', `/api/contacts/${id}`);
     request.addEventListener('load', () => {
-      console.log(this.url);
       if (request.status === 204) {
         console.log('delete successful');
       }
@@ -146,9 +159,83 @@ App = {
     request.send();
   },
 
+  cleanName(name) {
+    return name.trim().toLowerCase();
+  },
+
+  fetchMatches(query, callback) {
+    let request = new XMLHttpRequest();
+
+    request.open('GET', `${this.url}${encodeURIComponent(query)}`);
+    request.responseType = 'json';
+
+    request.addEventListener('load', () => {
+      callback(request.response);
+    });
+
+    request.send();
+  },
+
+  filterContactsByTags(e) {
+    if (e.target.style.backgroundColor === 'white') {
+      e.target.style.backgroundColor = 'lightgray';
+    } else if (e.target.style.backgroundColor = 'lightgray') {
+      e.target.style.backgroundColor = 'white';
+    }
+    
+    const tagKey = e.target.textContent;
+    this.tagList[tagKey] = !this.tagList[tagKey];
+    const filteredTags = Object.keys(this.tagList).filter(tag => this.tagList[tag] === true);
+    
+    this.filteredContacts = [];
+    for (let contact of this.contactList) {
+      for (let tag of filteredTags) {
+        if (contact.tags.includes(tag)) {
+          if (!this.filteredContacts.includes(contact)) {
+            this.filteredContacts.push(contact);
+          }
+        }
+      }
+    }
+    this.renderContacts(this.filteredContacts);
+  },
+
+  handleSearch() {
+    // TODO: if CMD-Backspace is clicked on, it does not wipe away the whole input row
+    const currentSearchStr = this.cleanName(this.searchInput.join(''));
+    const regex = new RegExp(`\w?${currentSearchStr}\w?`);
+    this.filteredContacts = this.contactList.filter(contact => this.cleanName(contact['full_name']).match(regex));
+    this.renderContacts(this.filteredContacts);
+  },
+
+  handleKeydown(e) {
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+      this.searchInput.push(e.key);
+      this.currentSearchIndex = this.searchInput.length;
+    } else if (e.key === 'Backspace') {
+      this.currentSearchIndex -= 1;
+      this.searchInput.splice(this.currentSearchIndex, 1);
+    } else if (e.key === 'ArrowLeft') {
+      if (this.currentSearchIndex <= 0) {
+        this.currentSearchIndex = 0;
+      } else if (this.currentSearchIndex > this.searchInput.length) {
+        this.currentSearchIndex = this.searchInput.length;
+      } else {
+        this.currentSearchIndex -= 1;
+      }
+    } else if (e.key === 'ArrowRight') {
+      if (this.currentSearchIndex < 0) {
+        this.currentSearchIndex = 0;
+      } else if (this.currentSearchIndex >= this.searchInput.length) {
+        this.currentSearchIndex = this.searchInput.length;
+      } else {
+        this.currentSearchIndex += 1;
+      }
+    }
+  },
+
   handleClick(e) {
     e.preventDefault();
-    console.log(e.target.id);
     if (e.target.id === "add-contact") {
       this.showFormTemplate();
     } else if (e.target.id === "cancel") {
@@ -163,39 +250,43 @@ App = {
         }
       };
     } else if (e.target.id === 'update-contact') {
-      console.log('update handle works');
+      this.form.querySelector('.contact-switch').textContent = "Update";
+      this.addContactBoolean = false;
       this.getExistingContact(e, this.loadCurrentContactInfo);
     } else if (e.target.id === 'delete-contact') {
       this.getExistingContact(e, this.deleteContact);
-    }
-  },
-
-  handleEnter(e) {
-    if (e.key === 'Enter') {
-      console.log('return key is pressed');
+    } else if (e.target.tagName === 'LI') {
+      this.filterContactsByTags(e);
     }
   },
 
   bindEvents() {
     document.addEventListener('click', this.handleClick.bind(this));
-    document.addEventListener('keypress', this.handleEnter.bind(this));
+    document.addEventListener('keydown', this.handleKeydown.bind(this));
+    document.querySelector('input').addEventListener('input', this.handleSearch.bind(this));
   },
 
-  async init() {
+  init() {
     this.addFormElements = document.querySelectorAll('.add-contact');
     this.submitForm = document.getElementById('submit');
     this.cancelFormElement = document.querySelectorAll('.cancel');
     this.form = document.getElementById('add');
     this.tagField = document.querySelector('.contact-tags');
+    this.tagCloudSearch = document.querySelector('.tag-search');
     this.url = 'http://localhost:3000';
     this.contactList;
     this.template;
     this.currentContact;
-    this.tagList = [];
+    this.tagList = {};
     this.addContactBoolean = true;
-    this.bindEvents();
     this.getAllContacts();
-    // this.getForm = await this.showFormTemplate();
+    this.searchInput = [];
+    this.currentSearchIndex = this.searchInput.length;
+    this.handleSearch = this.debounce(this.handleSearch.bind(this), 300);
+    this.bindEvents();
+    this.filteredContacts = this.contactList;
+    this.contactTemplate = document.getElementById('contact-template').innerHTML;
+    this.tagsClickedOn = [];
   },
 }
 
